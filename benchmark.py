df7d5af07d33d026dffffe55b65b85f0cb8510cb
GabrielNaka <gabriel.naka@unesp.br>
2026-09-19 16:17:56 -0300

    Adicionado nomes de autores, grafos do tipo gerador e DAG, .gitignore de pycache

--- benchmark.py
+++ benchmark.py
@@ -7,12 +7,19 @@ import statistics
 from dijkstra import Graph, dijkstra
 from heaps import PriorityQueue
 import time
+import math
 
 SEED = 2027
+DAGNodeProbability = 0.3
 
 heap_class = ['binary', 'binomial', 'fibonacci']
 tam_grafos = [10, 100, 500, 1000]
-fam_grafos = ['completo', 'dag', 'esparso']
+fam_grafos = [['Completo', 'gerar_grafoCompleto'],
+              ['Acíclico direcionado', 'gerar_grafoDAG'],
+              ['Gerador', 'gerar_arvoreGeradora']]
+
+
+
 
 # Função que gera um grafo completo e não direcionado(Kn), em que E é quase V²
 def gerar_grafoCompleto(n: int):
@@ -27,13 +34,66 @@ def gerar_grafoCompleto(n: int):
 
     return graph
 
-# Função para gerar um grafo acíclico e direcionado (DAG)
-def gerar_grafoDAG(n: int):
-    pass
 
-# Função para gerar um grafo esparso e não direcionado, cujo E == V
-def gerar_grafoEsparso(n: int):
-    pass
+# Função para gerar um grafo acíclico e direcionado (DAG)
+# Utilizando Algoritmo Erdős–Rényi G(n, p), com p = 0.3
+# Não é uniforme e não garante conectividade
+def gerar_grafoDAG(n: int):
+
+    graph = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(i + 1, n):
+            if (random.random() < DAGNodeProbability):
+                weight = round(random.uniform(1.0, 100.0), 1)
+                graph[i].append((j, weight))
+
+    return graph
+
+
+# Função para gerar uma árvore geradora, cujo E == V - 1
+# Utilizando Algoritmo de Wilson, o qual é uniforme
+def gerar_arvoreGeradora(n: int):
+
+    graph = [[] for _ in range(n)]
+
+    T = list() # Nós na arvore geradora
+    V = list(range(n)) # Nós restantes
+
+    current = random.choice(V)
+    T.append(current)
+    V.remove(current)
+
+
+    while len(T) < n:
+
+        # Escolher vértice aleatório não presente na árvore
+        path = [-1 for _ in range(n)]
+        current = random.choice(V)
+        first = current
+
+        # Executar random walk até encontrar árvore
+        while current not in T:
+            next = random.choice(range(n))
+            while next == current:
+                next = random.choice(range(n))
+
+            path[current] = next
+            current = next
+
+        # Apagar loops e adicionar random walk para árvore
+        current = first
+        while current not in T:
+            next = path[current]
+            weight = round(random.uniform(1.0, 100.0), 1)
+            graph[current].append((next, weight))
+            graph[next].append((current, weight))
+            T.append(current)
+            V.remove(current)
+            current = next
+
+    return graph
+
+
 
 # Mede a mediana e o desvio mediano em 7 execuções do algoritmo de dijkstra, dado um grafo e um tipo de heap
 def medir(graph: Graph, heap: str, repeticoes: int = 7) -> tuple[float, float]:
@@ -53,16 +113,12 @@ def main() -> None:
 
     for tam in tam_grafos:
         for tipo in fam_grafos:
-            if tipo == 'completo':
-                Graph = gerar_grafoCompleto(tam)
 
-            #elif tipo == 'dag':
-            #    Graph = gerar_grafoDAG(tam)
+            # Executa funcao em tipo
+            # tipo: [nome, função]
+            Graph = globals()[tipo[1]](tam)
 
-            #elif tipo == 'esparso':
-            #    Graph = gerar_grafoEsparso(tam)
-
-            print(f"Grafo: {tipo} com tamanho {tam}")
+            print(f"Grafo: {tipo[0]} com tamanho {tam}")
             for heap in heap_class:
                 print(f"{heap}: {medir(Graph, heap)}")
             print()
