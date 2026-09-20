e29cef4eca78c0e35bb5bd6889b5100bd54b052c
GabrielNaka <gabriel.naka@unesp.br>
2026-09-20 12:28:44 -0300

    grafo DK/push

--- graficos.py
+++ graficos.py
@@ -21,7 +21,13 @@ def find(list, graph, heap, column):
     return ret
 
 
-def plot(list, graph, column):
+def findWith(list, graph, heap, tam, column):
+    for row in list:
+        if row[0] == graph and row[1] == heap and str(row[2]) == str(tam):
+            return row[column + 3]
+
+
+def plotTempo(list, graph, column):
     plt.clf()
     for heap in heap_class:
         plt.plot (tam_grafos, find(list, graph, heap, column), label=heap)
@@ -34,6 +40,34 @@ def plot(list, graph, column):
     saida = "img/" + saida
     plt.savefig(saida)
     return saida
+
+
+def plotOperacoes(list):
+    plt.clf()
+
+    frac = []
+
+    # Pegando apenas string de nome em fam_grafos.
+    grafos = [x[0] for x in fam_grafos]
+
+    # Devido ao funcionamento de djikstra.py, relação de número de operações
+    # decrease_key / Push depende apenas do tipo de grafo utilizado, sendo
+    # constante em heap e tamanho. Por isso, fixou-se heap arbitrariamente
+    # e tamanho em um valor alto.
+    for tipo in grafos:
+        frac.append(float(findWith(list, tipo, heap_class[0], tam_grafos[-1], 4)) /
+                    float(findWith(list, tipo, heap_class[0], tam_grafos[-1], 2)))
+
+    plt.bar(grafos, frac)
+    plt.title('Relação Decrease-Key/Push')
+    plt.xlabel('Família de grafo')
+    plt.ylabel('DK/Push')
+
+    saida = "img/Relação DK-Push"
+    plt.savefig(saida)
+    return saida
+
+
 
 
 
@@ -50,5 +84,7 @@ with open('benchmarking.csv', mode='r', encoding='utf-8') as file:
 
 
 for grafo in fam_grafos:
-    for i in range(0, 5):
-        print ("Arquivo criado: " + plot(benchmarking, grafo[0], i))
+    for i in range(0, 2):
+        print ("Arquivo criado: " + plotTempo(benchmarking, grafo[0], i))
+
+print ("Arquivo criado: " + plotOperacoes(benchmarking))
